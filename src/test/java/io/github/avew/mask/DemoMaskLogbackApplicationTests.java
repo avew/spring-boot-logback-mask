cb516ce5ff84f9e0de65ced253c8ea1456225132
asep.rojali <asep.rojali@pajakku.com>
2025-02-12 12:08:24 +0700

    first commit

---
+++ src/test/java/io/github/avew/mask/DemoMaskLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.avew.mask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMaskLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
